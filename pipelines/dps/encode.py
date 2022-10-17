76e4e5f9686d34a32aa52842ecb859e93758f295
Martin Bayly <Martin.Bayly@D2L.com>
2022-10-17 07:07:56 -0700

    fix update expression

--- pipelines/dps/encode.py
+++ pipelines/dps/encode.py
@@ -321,7 +321,7 @@ def update_pipeline_count(table):
 				'pk': 'CONTROL1',
 				'sk': 'PIPELINE'
 			},
-			UpdateExpression='SET tally = tally + :inc_1 SET updatedAt = :updated_at',
+			UpdateExpression='SET tally = tally + :inc_1, updatedAt = :updated_at',
 			ExpressionAttributeValues={
 				':inc_1': 1,
 				':updated_at': f"{datetime.utcnow().isoformat()[:-3]}Z"
